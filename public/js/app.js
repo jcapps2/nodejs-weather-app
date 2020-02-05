32ee5140c04d9e6fd985f8957734ce5162936d06
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-02-05 11:55:15 -0600

    Setup app for Heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()          // prevents browser refresh on button click
@@ -13,8 +12,10 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''             // Clears previous search when button is clicked again
 
-    // Basically fetch data from this location, and then run this function
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    // Basically fetch data from this location, and then run this function.
+    // Removed http://localhost:3000 because we can't do that once deployed on Heroku.
+    // So the following '/weather?address=' + location will be concatenated onto Heroku url.
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
